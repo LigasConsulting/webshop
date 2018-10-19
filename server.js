f2d7677e2bce28a6babfa2926cb897448e57bed7
Miroslav Ligas <miroslav@ligas.im>
2018-10-19 23:54:15 +0100

    direct linking fix

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-var prerenderNode = require('prerender-node')
+var express = require('express');
+var prerenderNode = require('prerender-node');
+const handler = require('serve-handler');
 
-var app = express()
+var app = express();
 const port = 5000;
 
 app.use(prerenderNode
-    .set('prerenderToken', 'WRXhbE5LQL0DZEsbeNS2')
+        .set('prerenderToken', 'WRXhbE5LQL0DZEsbeNS2')
     // .set('prerenderServiceUrl', 'http://localhost:3000/')
 );
-app.use(serveStatic(path.join(__dirname, 'build')));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use((req, res, next) => {
+    return handler(req, res, {
+        "public": "build",
+        "rewrites": [{
+            source: '**',
+            destination: '/index.html'
+        }]
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+});
